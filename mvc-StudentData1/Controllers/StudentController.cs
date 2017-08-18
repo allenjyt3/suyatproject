8d584c930f54df7ec85cb7502cf4eacaa7b7d960
SUYATITECH\trainee <joviesvarghese@gmail.com>
2017-08-18 15:46:27 +0530

    Add project files.

---
+++ mvc-StudentData1/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_StudentData1.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            // string[] strStudent = new string[4];
+            // strStudent[0] = "a";
+            // strStudent[1] = "b";
+            // strStudent[2] = "b";
+            // strStudent[3] = "d";
+            //  return (strStudent);
+
+            ViewBag.Name = "aaaa";
+            return View();
+
+
+            
+            
+        }
+        
+    }
+}
---
+++ mvc-StudentData1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_StudentData1.Startup))]
+namespace mvc_StudentData1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
